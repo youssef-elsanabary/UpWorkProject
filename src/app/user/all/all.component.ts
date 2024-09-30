2f7a24027be1df00317db5acbc65e710b29ef798
youssef-elsanabary <youssefelsanabary19@gmail.com>
2024-09-30 19:04:23 +0300

    mainComponent

---
+++ src/app/user/all/all.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all',
+  standalone: true,
+  imports: [],
+  templateUrl: './all.component.html',
+  styleUrl: './all.component.css'
+})
+export class AllComponent {
+
+}
